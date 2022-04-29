bf5840af7f861d56ef38b3b6fce5576b86740d79
Ramiza1998 <101269986+Ramiza1998@users.noreply.github.com>
2022-04-29 08:17:48 -0700

    Add files via upload

---
+++ src/context/Auth.context.js
@@ -0,0 +1,82 @@
+import { useState, createContext } from "react";
+
+const AuthContext = createContext();
+
+const AuthProvider = AuthContext.Provider;
+function AuthContextProvider({ children }) {
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const authLogin = async (email, password) => {
+    await fetch("/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setSuccess(true);
+        setUser(data);
+      })
+      .catch((err) => {
+        setError(err);
+      });
+    setLoading(false);
+  };
+
+  //sign up
+  const authRegister = async (username, email, password) => {
+    await fetch("/api/users/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setSuccess(true);
+        setUser(data);
+      })
+      .catch((err) => {
+        setError(err);
+      });
+    setLoading(false);
+  };
+
+  //log out
+  const AuthLogout = () => {
+    setSuccess(false);
+    setUser(null);
+  };
+  return (
+    <AuthProvider
+      value={{
+        success,
+        error,
+        loading,
+        user,
+        authLogin,
+        authRegister,
+        AuthLogout,
+      }}
+    >
+      {children}
+    </AuthProvider>
+  );
+
+  return <AuthProvider value={{}}>{children}</AuthProvider>;
+}
+
+export { AuthContext, AuthContextProvider };
